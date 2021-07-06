8464ede4d403e159c007c8fbc2ec6b1bc8ad6ab3
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-05 19:05:13 -0700

    URL Shortener Microservice attempt 1

--- server.js
+++ server.js
@@ -2,23 +2,42 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser'); // middleware
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+//on form submission, check url & respond w/error or short url
+var short;
+var long;
+app.post("/api/shorturl", function (req, res) {
+  short = Math.floor(Math.random() * 1000) + 1;
+  long = req.body.url;
+  try {
+    url = new URL(long);
+  } catch (_) {
+    res.json({ error: 'invalid url' })
+    return;
+  }
+  res.json({ original_url: long, short_url: short })
 });
 
-app.listen(port, function() {
+//on submission of short url, redirect to long
+app.get('/api/shorturl/:short', (req, res) => {
+  res.redirect(long);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
